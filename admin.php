b4c16f01d82d3fa6ec4152c2fc5e0a1310cca0e7
Rody10 <36506046+Rody10@users.noreply.github.com>
2021-10-07 16:51:53 +0200

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+    <link rel="stylesheet" href="admin.css">
+<link rel="icon" type="image/x-icon" href="images/blueico.ico">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+
+    <body>
+<div class="nav-bar">
+  <div class="nav-logo">
+      <img src="images/logo2.png">
+  </div>
+  <div class="nav-links">
+    <ul>
+        <a href='#'><li>ADMIN</li></a>
+    </ul>
+</div>
+</div>
+
+  <div class="wrapper">
+    <h1>Your Utilities:</h1>
+  </div>
+  <div class="table">
+    <table>
+      <tr>
+        <td><a href="maintainBooking.php"><i class="fa fa-book" style="font-size: 25px;"></i>Bookings</a></td>
+        <td><a href="bedRequestReport.php"><i class="fa fa-bed" style="font-size: 25px;"></i>Bed Requests</a></td>
+      </tr>
+      <tr>
+        <td><a href="MaintainDepot.php"><i class="fa fa-home" style="font-size: 25px;"></i>Depots</a></td>
+        <td><a href="maintainClient.php"><i class="fa fa-user" style="font-size: 25px;"></i>Clients</a></td>
+        
+        
+      </tr>
+      <tr>
+      <td><a href="home.html"><i class="fa fa-sign-out" style="font-size: 25px;"></i>Log Out</a></td>
+      </tr>
+    </table>
+  </div>
+  
+</body>
+</html>
